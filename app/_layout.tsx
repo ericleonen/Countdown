43e874d863904da85eb5c17db6cf33b47172cac5
ericleonen <ericleonen@gmail.com>
2024-12-22 14:01:08 -0800

    create countdown screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+	return (
+		<Stack>
+			<Stack.Screen name="index" options={{ title: "Embarrassing Countdown" }} />
+			<Stack.Screen name="countdown" options={{ title: "Countdown" }} />
+		</Stack>
+  	);
 }
---
+++ app/countdown.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function CountdownScreen() {
+    return (
+        <View>
+            <Text>Countdown screen</Text>
+        </View>
+    )
+}
